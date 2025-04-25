77a2419bc1c106033c20e68d503d61f445a54043
Zahas-1 <127893806+Zahas-1@users.noreply.github.com>
2025-04-25 09:21:29 +0100

    Add files via upload

---
+++ src/main/java/EvenOrOdd.java
@@ -0,0 +1,11 @@
+/*
+Create a function that takes an integer as an argument and returns
+"Even" for even numbers or "Odd" for odd numbers.
+
+ */
+public class EvenOrOdd {
+    public static String evenOrOdd(int number) {
+        return number % 2.0 == 0 ? "Even" : "Odd";
+    }
+
+}
