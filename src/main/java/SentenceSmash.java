67ac88f5f60ad8adc84969db6b3af3f066cdac1f
Zahas-1 <127893806+Zahas-1@users.noreply.github.com>
2025-08-01 08:48:06 +0100

    Add files via upload

---
+++ src/main/java/SentenceSmash.java
@@ -0,0 +1,23 @@
+/*
+Write a function that takes an array of words and smashes them together into
+a sentence and returns the sentence. You can ignore any need to sanitize words
+or add punctuation, but you should add spaces between each word. Be careful,
+there shouldn't be a space at the beginning or the end of the sentence!
+
+Example
+['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+ */
+
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+
+public class SentenceSmash {
+    public static String smash(String... words) {
+        return Arrays.stream(words)
+                .collect(Collectors.joining(" "));
+    }
+
+}
